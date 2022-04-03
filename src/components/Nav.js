d2d1b3400d25b42947ecab61bbdba2524cfda271
steven <steven_ko@paradise-soft.com.tw>
2022-04-03 15:47:51 +0800

    feat: initial setup

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href='/'>Home</a>
+        </li>
+        <li>
+          <a href='/about'>About</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Search from '../components/Search'
+
+const Homepage = () => {
+  const auth = '563492ad6f917000010000017d242b38f3d149c4a21bcdba5326d52d'
+  return (
+    <div>
+      <h1>This is homepage.</h1>  
+    </div>
+  )
+}
+
+export default Homepage
